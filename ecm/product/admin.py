13c59d562215fd931f0d12d5fe661cb39ef35f1a
Md Eyan Parvez Mullick <73710494+ayanmalik04@users.noreply.github.com>
2025-06-11 20:22:37 +0530

    Update admin.py

--- ecm/product/admin.py
+++ ecm/product/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import productt , Payment , Review , SalesReport
+from .models import productt , Payment , Review 
 # Register your models here.
 admin.site.register(productt)
 admin.site.register(Payment)
 admin.site.register(Review)
 
 
-class SalesReportAdmin(admin.ModelAdmin):
-    list_display = ('report',)
+# class SalesReportAdmin(admin.ModelAdmin):
+#     list_display = ('report',)
 
-admin.site.register(SalesReport, SalesReportAdmin)
+# admin.site.register(SalesReport, SalesReportAdmin)
 
 
 
